f3c674a258f95129af94e4d5372ea551db93416e
Christopher Nguyen <Chris_ngu@pm.me>
2020-02-12 20:49:01 -0600

    reducer html calls, adding action calls

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {GET_LEADS} from '../../actions/leads';
 
 export class Leads extends Component {
+
+    static propTypes = {
+        leads: PropTypes.array.isRequired
+    }
+
     render() {
         return (
             <div>
                 <h1> LEADS LIST HERE</h1>
             </div>
-        )
+        );
     }
 }
 
-export default Leads
+const mapStateToPRops = state => ({
+    leads: state.leads.leads //leads reducer -> state of lead
+});
+
+export default connect(mapStateToPRops)(Leads);
